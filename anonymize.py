a22241447e79a6898a5ec2e50abc01278bcaa6da
Evi Vanoost <vanooste@rcbi.rochester.edu>
2022-08-05 17:36:21 -0400

    Refactoring and commenting

--- anonymize.py
+++ anonymize.py
@@ -21,16 +21,32 @@ from pydicom.errors import InvalidDicomError
 from dcm_functions import hashtext, time2str, date2str, datetime2str, str2time, str2date, str2datetime, \
     regenuid
 
+# Define where our incoming and outgoing data are going to live
+# I am using environment variables here to be able to modify using Docker
+# The peculiar name picked is because we want to also use this in conjunction with Mercure DICOM router
 INCOMING_DIR = os.environ.get("MERCURE_IN_DIR", "/in")
 OUTGOING_DIR = os.environ.get("MERCURE_OUT_DIR", "/out")
 
+# Whether we want Mercure-specific things such as task.json to load. If not, set to False
+DO_MERCURE_STUFF = os.environ.get("DO_MERCURE_STUFF", True)
+
+# Find out where "we" live at runtime. This increases portability.
 app_path = Path(__file__).parent.absolute()
+# config should be a directory in the current app directory
 config_path = os.path.join(app_path, "config")
 
+# Check whether our incoming and outgoing paths actually exist. A common mistake if using Docker.
+# Reason we're not just blindly going forward, is because we could create the directories in locations we don't want
 if not Path(INCOMING_DIR).exists() or not Path(OUTGOING_DIR).exists():
     print("IN/OUT paths do not exist")
     sys.exit(1)
 
+# Check and see whether we have a studies and stations configuration
+# studies.json contains the study de-identification configuration
+# stations.json contains the configuration to decode the study name from a particular scanner
+# Every MRI system (Philips, GE, Siemens) and sometimes even institutions have their own 'way' of doing things
+# random.txt contains a random seed that initializes the randomizer that we should keep secret
+# task.json is Mercure specific.
 try:
     with open(os.path.join(config_path, 'studies.json'), 'r') as json_file:
         studies: dict = json.load(json_file)
@@ -41,26 +57,33 @@ try:
     with open(os.path.join(config_path, 'random.txt'), 'r') as file:
         RANDOM_UUID: str = file.read().replace('\n', '')
 
-    with open(Path(INCOMING_DIR) / "task.json", "r") as json_file:
-        task = json.load(json_file)
+    if DO_MERCURE_STUFF:
+        with open(Path(INCOMING_DIR) / "task.json", "r") as json_file:
+            task = json.load(json_file)
 except FileNotFoundError:
     print("No studies.json, stations.json, random.txt and/or task.json found")
     sys.exit(1)
 
-if task.get("process", False):
+# Mercure can override our standard configuration from Mercure
+if DO_MERCURE_STUFF and task.get("process", False):
     settings = task["process"].get("settings", False)
     if settings:
+        # Here we override (merge) our settings with Mercure's settings
         RANDOM_UUID = settings.get("RANDOM_UUID", RANDOM_UUID)
         studies.update(settings.get("studies", {}))
         stations.update(settings.get("stations", {}))
 
-ORIGINAL_UUID = RANDOM_UUID
+# Keep track of when we start
 NOW = datetime.now()
 
+# Empty series
 series = {}
+
+# Scan the incoming directory for files
 for entry in os.scandir(INCOMING_DIR):
+    # Filter out DICOM files
     if entry.name.endswith(".dcm") and not entry.is_dir():
-        # Get the Series UID from the file name
+        # Get the Series UID from the file name, this is based on the DCMTK storescp naming convention
         seriesString = entry.name.split("#", 1)[0]
         # If this is the first image of the series, create new file list for the series
         if seriesString not in series.keys():
@@ -68,37 +91,211 @@ for entry in os.scandir(INCOMING_DIR):
         # Add the current file to the file list
         series[seriesString].append(entry.name)
 
+# Nothing to be done
 if not series:
     print("No files found to be processed")
     sys.exit(0)
 
 
-def get_key_types(studyinfo, action_type='value'):
-    listOfKeys = list()
-    listOfItems = studyinfo['AnonymizeTag'].items()
-    for item in listOfItems:
+# Helper function to get key types of a specific action type from the study configuration
+def get_key_types(study_info, action_type):
+    # This will be our output
+    list_of_keys = []
+
+    for item in study_info['AnonymizeTag'].items():
         if item[1]['action'] == action_type:
-            listOfKeys.append(item[0])
+            list_of_keys.append(item[0])
 
-    return listOfKeys
+    return list_of_keys
 
 
-def get_vr_action(vr):
-    if vr in VRDefaults:
-        return VRDefaults[vr].get('action', 'delete')
+# Helper function to get the default action on a VR (AnonymizeVR in JSON)
+def get_vr_action(this_vr, study_info_vr):
+    # If this DICOM tags' VR is in the studies list of VRs
+    if this_vr in study_info_vr:
+        # If no action is specified, delete
+        return study_info_vr[this_vr].get('action', 'delete')
     # The default action is to keep the value
     return 'keep'
 
 
-def get_tag_action(tag):
-    if tag in TagDefaults:
-        return TagDefaults[tag].get('action', 'delete')
+# Helper function to get the action on a specific tag (AnonymizeTag in JSON)
+def get_tag_action(this_tag, study_info_tag):
+    # If this DICOM tag is listed in the AnonymizeTag
+    if this_tag in study_info_tag:
+        return study_info_tag[this_tag].get('action', 'delete')
     # The default action is to keep the value
     return 'keep'
 
-for oldseriesuid in series:
-    for filename in series[oldseriesuid]:
-        # Make sure we have a valid DICOM
+
+def anonymize_dicom_file(this_dataset, global_random_uuid, study_info):
+    # Keep track of any private tags to save
+    saved_private_tags = {}
+
+    # If the Remove Private Tags flag is set
+    if study_info.get('RemovePrivateTags', True):
+        print("Removing Private Tags")
+        # Preserve certain tags
+        for tag in study_info.get('SavePrivateTags', []):
+            print(f"Preserving Private Tag: {tag}")
+            try:
+                saved_private_tags[tag] = this_dataset[tag]
+            except KeyError:
+                print(f"Private Tag does not exist: {tag}")
+
+        this_dataset.remove_private_tags()
+
+        # Restore private tags
+        for tag in saved_private_tags:
+            this_dataset[tag] = saved_private_tags[tag]
+
+    # See if our Study overrides our default random seed
+    study_random_uuid = study_info.get('RandomSeed', global_random_uuid)
+    # Values should always be set
+    tags_to_set = get_key_types(study_info, 'value')
+    for tag in tags_to_set:
+        value = study_info['AnonymizeTag'][tag].get('value', None)
+        if value == "TMnow":
+            value = time2str(NOW)
+        elif value == "DAnow":
+            value = date2str(NOW)
+        elif value == "DTnow":
+            value = datetime2str(NOW)
+
+        # This apparently overwrites existing tags
+        this_dataset.add_new(tag, dictionary_VR(tag), value)
+
+    # pydicom dataset is a dict which contains DataElement instances
+    for data_element in this_dataset.iterall():
+        # Check whether this data_element is a valid DICOM tag (is in pydicom data dictionary)
+        try:
+            tag = pydicom.datadict.get_entry(data_element.tag)[4]
+        except KeyError:
+            print(f"{data_element.tag} -> keep (Invalid Tag)")
+            continue
+
+        # Get the action for the entire VR
+        action = get_vr_action(data_element.VR, study_info['AnonymizeVR'])
+        tag_action = get_tag_action(tag, study_info['AnonymizeTag'])
+        # The tag action overrides VR
+        if tag_action:
+            action = tag_action
+
+        print(f"{tag} -> {action}")
+
+        # Define each action
+        # We need to keep the current tag as-is
+        if action == "keep":
+            # Do nothing
+            continue
+
+        # Delete the current tag
+        if action == "delete":
+            try:
+                delattr(this_dataset, tag)
+            except AttributeError:
+                # It is possible that nested tags may be deleted
+                print(f"{tag} no longer exists - parent deleted?")
+            continue
+
+        # Clear (keeps the tag, but sets the tag value to nothing)
+        if action == "clear":
+            try:
+                data_element.value = None
+            except KeyError:
+                print(f"{tag} no longer exists - parent deleted?")
+            continue
+
+        # Hash the value of the tag
+        if action == "hash":
+            salt = study_info['AnonymizeTag'][tag].get('salt', study_random_uuid)
+            try:
+                data_element.value = hashtext(this_dataset.data_element(tag).value, salt)
+            except KeyError:
+                print(f"{tag} no longer exists - parent deleted?")
+            continue
+
+        # Offset the integer value within the tag by a set amount
+        if action == "offset":
+            amount = study_info['AnonymizeTag'][tag].get('delta', None)
+            if not amount:
+                seed = study_info['AnonymizeTag'][tag].get('seed', study_random_uuid)
+                # Do not use the value for offsetting as we want everything to be offset by the same value
+                random.seed(seed)
+                # 100 year variation should be sufficient
+                amount = random.randint(-1576800000, 1576800000)
+
+            # AS: TODO (Age String nnnD, nnnW, nnnM, nnnY)
+            # DS: TODO (Decimal String "0"-"9", "+", "-", "E", "e", ".")
+            # FL: TODO (Floating Point Single)
+            # FD: TODO (Floating Point Double)
+            # IS: TODO (Integer String)
+            # SL: TODO (Signed Long)
+            # SS: TODO (Signed Short)
+            # UL: TODO (Unsigned Long)
+            # US: TODO (Unsigned Short)
+
+            if data_element.VR == "TM":
+                time = str2time(data_element.value)
+                # List = h, m, s, ms
+                time = time + timedelta(seconds=amount)
+                data_element.value = time2str(time)
+                continue
+
+            if data_element.VR == "DA":
+                time = str2date(data_element.value)
+                # List = h, m, s, ms
+                time = time + timedelta(seconds=amount)
+                data_element.value = date2str(time)
+                continue
+
+            if data_element.VR == "DT":
+                time = str2datetime(data_element.value)
+                # List = h, m, s, ms
+                time = time + timedelta(seconds=amount)
+                data_element.value = datetime2str(time)
+                continue
+
+            print(f"{tag} -> Changed from {action} to keep (Error)")
+            print(f"ERROR: Offsetting a {data_element.VR} Not Implemented")
+            continue
+
+        if action == "regen":
+            seed = study_info['AnonymizeTag'][tag].get('seed', study_random_uuid)
+            # Use the value as the seed as we are simply regenerating, not offsetting
+            random.seed(seed + data_element.value)
+            # 100 year variation should be sufficient
+            amount = random.randint(-1576800000, 1576800000)
+            time = NOW + timedelta(seconds=amount)
+            if data_element.VR == "TM":
+                data_element.value = time2str(time)
+            elif data_element.VR == "DA":
+                data_element.value = date2str(time)
+            elif data_element.VR == "DT":
+                data_element.value = datetime2str(time)
+            else:
+                data_element.value = regenuid(data_element.value, seed)
+            continue
+
+        print(f"{tag} -> Changed from {action} to keep (Error)")
+        print(f"ERROR: Action Not Implemented: {action}")
+
+    # We need to always regen some File MetaData that is identifiable
+    this_dataset.file_meta.MediaStorageSOPInstanceUID = regenuid(this_dataset.file_meta.MediaStorageSOPInstanceUID,
+                                                                 study_random_uuid)
+    # By convention, DICOM files provided by
+    # mercure have the format [series_UID]#[file_UID].dcm
+    series_instance_uid = str(this_dataset.data_element("SeriesInstanceUID").value)
+    file_uid = this_dataset.file_meta.MediaStorageSOPInstanceUID
+    new_filename = os.path.join(OUTGOING_DIR, f"{series_instance_uid}#{file_uid}.dcm")
+    this_dataset.save_as(new_filename)
+
+
+# Loop through the series, we name it old_series_uid because we need to deidentify uid
+for old_series_uid in series:
+    # Loop through each file in the filename
+    for filename in series[old_series_uid]:
+        # Make sure the file we have is a valid DICOM. pydicom bubbles up an exception
         try:
             dataset = pydicom.filereader.dcmread(f"{INCOMING_DIR}/{filename}")
         except InvalidDicomError:
@@ -106,7 +303,8 @@ for oldseriesuid in series:
                   " is missing DICOM File Meta Information header or the 'DICM' prefix is missing from the header.")
             continue
 
-        # Find Station Name, each Station has different methods of encoding Study Names
+        # Find StationName in the DICOM file
+        # Each Station may have different methods of encoding Study Names
         try:
             StationName = str(dataset.data_element('StationName').value).upper()
             StationInfo = stations.get(StationName, stations.get("default"))
@@ -137,141 +335,4 @@ for oldseriesuid in series:
         print("Processing: " + filename)
         print("Study: " + StudyName)
 
-        SAVED_TAGS = {}
-        REMOVE_PRIVATE_TAGS = StudyInfo.get('RemovePrivateTags', True)
-        SAVE_PRIVATE_TAGS = StudyInfo.get('SavePrivateTags', [])
-        # If the Remove Private Tags flag is set
-        if REMOVE_PRIVATE_TAGS:
-            print("Removing Private Tags")
-            # Preserve certain tags
-            for tag in SAVE_PRIVATE_TAGS:
-                print(f"Preserving Private Tag: {tag}")
-                try:
-                    SAVED_TAGS[tag] = dataset[tag]
-                except KeyError:
-                    print(f"Private Tag does not exist: {tag}")
-
-            dataset.remove_private_tags()
-
-            # Restore private tags
-            for tag in SAVED_TAGS:
-                dataset[tag] = SAVED_TAGS[tag]
-
-        TagDefaults = StudyInfo['AnonymizeTag']
-        VRDefaults = StudyInfo['AnonymizeVR']
-        RANDOM_UUID = StudyInfo.get('RandomSeed', ORIGINAL_UUID)
-
-        # Values should always be set
-        tags_to_set = get_key_types(StudyInfo, 'value')
-        for tag in tags_to_set:
-            value = TagDefaults[tag].get('value', None)
-            if value == "TMnow":
-                value = time2str(NOW)
-            elif value == "DAnow":
-                value = date2str(NOW)
-            elif value == "DTnow":
-                value = datetime2str(NOW)
-
-            # This apparently overwrites existing tags
-            dataset.add_new(tag, dictionary_VR(tag), value)
-
-        for data_entry in dataset.iterall():
-            try:
-                tag = pydicom.datadict.get_entry(data_entry.tag)[4]
-            except KeyError:
-                print(f"{data_entry.tag} -> keep (Invalid Tag)")
-                continue
-
-            # We can take action on the entire VR
-            action = get_vr_action(data_entry.VR)
-            # The tag action overrides VR
-            action = get_tag_action(tag)
-            print(f"{tag} -> {action}")
-
-            if action == "keep":
-                # Do nothing
-                continue
-
-            if action == "delete":
-                try:
-                    delattr(dataset, tag)
-                except AttributeError:
-                    print(f"{tag} no longer exists - parent deleted?")
-                continue
-
-            if action == "clear":
-                data_entry.value = None
-                continue
-
-            if action == "hash":
-                salt = TagDefaults[tag].get('salt', RANDOM_UUID)
-                try:
-                    data_entry.value = hashtext(dataset.data_element(tag).value, salt)
-                except KeyError:
-                    print(f"{tag} no longer exists - parent deleted?")
-                continue
-
-            if action == "offset":
-                amount = TagDefaults[tag].get('delta', None)
-                if not amount:
-                    seed = TagDefaults[tag].get('seed', RANDOM_UUID)
-                    # Do not use the value for offsetting as we want everything to be offset by the same value
-                    random.seed(seed)
-                    # 100 year variation should be sufficient
-                    amount = random.randint(-1576800000, 1576800000)
-
-                if data_entry.VR == "TM":
-                    time = str2time(data_entry.value)
-                    # List = h, m, s, ms
-                    time = time + timedelta(seconds=amount)
-                    data_entry.value = time2str(time)
-                    continue
-
-                if data_entry.VR == "DA":
-                    time = str2date(data_entry.value)
-                    # List = h, m, s, ms
-                    time = time + timedelta(seconds=amount)
-                    data_entry.value = date2str(time)
-                    continue
-
-                if data_entry.VR == "DT":
-                    time = str2datetime(data_entry.value)
-                    # List = h, m, s, ms
-                    time = time + timedelta(seconds=amount)
-                    data_entry.value = datetime2str(time)
-                    continue
-
-                print(f"{tag} -> Changed from {action} to keep (Error)")
-                print(f"ERROR: Offsetting a {data_entry.VR} Not Implemented")
-                continue
-
-            if action == "regen":
-                seed = TagDefaults[tag].get('seed', RANDOM_UUID)
-                # Use the value as the seed as we are simply regenerating, not offsetting
-                random.seed(seed + data_entry.value)
-                # 100 year variation should be sufficient
-                amount = random.randint(-1576800000, 1576800000)
-                time = NOW + timedelta(seconds=amount)
-                if data_entry.VR == "TM":
-                    data_entry.value = time2str(time)
-                elif data_entry.VR == "DA":
-                    data_entry.value = date2str(time)
-                elif data_entry.VR == "DT":
-                    data_entry.value = datetime2str(time)
-                else:
-                    data_entry.value = regenuid(data_entry.value, seed)
-                continue
-
-            print(f"{tag} -> Changed from {action} to keep (Error)")
-            print(f"ERROR: Action Not Implemented: {action}")
-
-        # We need to always regen some File MetaData that is identifiable
-        dataset.file_meta.MediaStorageSOPInstanceUID = regenuid(dataset.file_meta.MediaStorageSOPInstanceUID,
-                                                                RANDOM_UUID)
-
-        # By convention, DICOM files provided by
-        # mercure have the format [series_UID]#[file_UID].dcm
-        SeriesInstanceUID = str(dataset.data_element("SeriesInstanceUID").value)
-        FileUID = dataset.file_meta.MediaStorageSOPInstanceUID
-        newfilename = os.path.join(OUTGOING_DIR, f"{SeriesInstanceUID}#{FileUID}.dcm")
-        dataset.save_as(newfilename)
+        anonymize_dicom_file(dataset, RANDOM_UUID, StudyInfo)
