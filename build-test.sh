a64c958b7dcb12b07ec78b629a57fbd14e2788ee
Evi Vanoost <vanooste@rcbi.rochester.edu>
2022-10-05 10:16:37 -0400

    Make potential changes more clear

--- build-test.sh
+++ build-test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+IMAGE="guruevi/dcmanonymizer"
 docker rm dcmanonymizer_test
 rm -rf test-data/out/*
 cp config/* myconfig/
@@ -6,6 +7,6 @@ docker run --mount type=bind,source="$(pwd)"/test-data/in,target=/in \
            --mount type=bind,source="$(pwd)"/test-data/out,target=/out \
            --mount type=bind,source="$(pwd)"/myconfig,target=/app/config \
            --name dcmanonymizer_test \
-           -d guruevi/dcmanonymizer
+           -d ${IMAGE}
 sleep 5
 docker logs dcmanonymizer_test
